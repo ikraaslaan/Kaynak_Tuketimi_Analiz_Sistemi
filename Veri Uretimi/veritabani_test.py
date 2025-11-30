09d892b03850dd92c3d72341153603c924c8a91f
Ahmet-Sarraf <srrfahmet@gmail.com>
2025-11-30 11:44:03 +0300

    Veri tabanı bağlantısı testi için yeni modül

---
+++ Veri Uretimi/veritabani_test.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+
+try:
+    client = MongoClient("mongodb+srv://23frontend23_db_user:PaoDBStFSwY3nPR0@verikaynagi.bueal8j.mongodb.net")
+    client.admin.command("ping")
+    print("MongoDB bağlantısı başarılı!")
+except Exception as e:
+    print("Hata:", e)
