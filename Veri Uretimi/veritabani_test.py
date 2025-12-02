2b55b8ca4d12d69b4a77d1fd7dfe506921c91198
Ahmet-Sarraf <srrfahmet@gmail.com>
2025-12-02 17:52:04 +0300

    Veri tabanı bağlantısı silindi, yapı hala duruyor

--- Veri Uretimi/veritabani_test.py
+++ Veri Uretimi/veritabani_test.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 
 try:
-    client = MongoClient("mongodb+srv://23frontend23_db_user:PaoDBStFSwY3nPR0@verikaynagi.bueal8j.mongodb.net")
+    client = MongoClient("Mongodb bağlantısını yazınız!")
     client.admin.command("ping")
     print("MongoDB bağlantısı başarılı!")
 except Exception as e:
